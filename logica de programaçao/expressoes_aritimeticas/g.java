8abbf06b487987eb9e3eb6d5a4ac9b532a17df5f
Geneton Neto <geneton.alves.neto@gmail.com>
2023-05-11 19:18:52 -0300

    questoes de expressao numerica, iphac

---
+++ logica de programaçao/expressoes_aritimeticas/g.java
@@ -0,0 +1,18 @@
+package expressoes_aritimeticas;
+
+public class g {
+
+	public static void main(String[] args) {
+
+	//g) C * D + A * B
+	
+	int a = 5;
+	int b = 10;
+	int c = 8;
+	double d = 1.5; 
+		
+	double resultado = c * d + a * b;
+	System.out.println(resultado);
+	
+	}
+}
