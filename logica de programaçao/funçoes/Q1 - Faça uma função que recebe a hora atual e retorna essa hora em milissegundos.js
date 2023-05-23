a6ba4d2d6b656a1d62383b3e5ff0f886e03ab3ce
Geneton Neto <geneton.alves.neto@gmail.com>
2023-05-23 09:19:52 -0300

    atividade 04, funçoes

---
+++ logica de programaçao/funçoes/Q1 - Faça uma função que recebe a hora atual e retorna essa hora em milissegundos.js
@@ -0,0 +1,8 @@
+// Faça uma função que recebe a hora atual e retorna essa hora em milissegundos.
+
+function parametro(hora, horaMilissegundos) {
+    return hora * horaMilissegundos;
+}
+
+var resultado = parametro(2, 3600000);
+console.log(resultado);
---
+++ logica de programaçao/funçoes/Q3 - Faça uma função que recebe, por parâmetro, dois valores X e Z e calcula e retorna Xz.js
@@ -0,0 +1,8 @@
+// Faça uma função que recebe, por parâmetro, dois valores X e Z e calcula e retorna Xz..
+
+function parametro(x,z) {
+    return (x + z);
+}
+
+var resultado = parametro("X","z");
+console.log(resultado);
